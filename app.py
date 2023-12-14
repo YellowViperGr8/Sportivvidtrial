9f23723350b7d818ff636a195daafacadf27acee
YellowViperGr8 <149850569+YellowViperGr8@users.noreply.github.com>
2023-12-14 16:03:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,13 +92,8 @@ def anglesp(lmlist, points, lines, drawpoints):
 def process_videop():
     global cap_pushup, pd_pushup, img, counterp, directionp, video_access_event_pushup
 
-    while video_access_event_pushup.is_set():
-     select.select((cap_pushup,), (), ())
-     image_data = cap_pushup.read_and_queue()
-     image = np.frombuffer(image_data, dtype=np.uint8)
-     ret, frame = cv2.imdecode(image)
-     #while(True):
-        # ret, frame = cap_pushup.read()
+     while(True):
+        ret, frame = cap_pushup.read()
 
      if ret:
          frame = cv2.flip(frame, 1)
