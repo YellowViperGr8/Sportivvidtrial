1f3dfd2157023063d05a3b393fb728237565df95
YellowViperGr8 <149850569+YellowViperGr8@users.noreply.github.com>
2023-12-15 18:19:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,6 @@ def anglesp(lmlist, points, lines, drawpoints):
 
 
 
-
 def generate_frames(stream):
 
             
@@ -149,26 +148,24 @@ def generate_frames(stream):
 
     global video, pd_pushup, img, counterp, directionp, video_access_event_pushup, stop_video_flag
 
-    cap = cv2.VideoCapture(stream)
-
     while True:
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        frame = cv2.flip(frame, 1)
-        img = cv2.resize(frame, (1000, 500))
-        cvzone.putTextRect(img, 'AI Push Up Counter', [345, 30], thickness=2, border=2, scale=2.5)
-        pd_pushup.findPose(img, draw=0)
-        lmlist, _ = pd_pushup.findPosition(img, draw=0, bboxWithHands=0)
-
-        anglesp(lmlist, [lmlist[p] for p in (11, 13, 15, 12, 14, 16)], [(11, 13, 6), (13, 15, 6), (12, 14, 6),
-                                                                        (14, 16, 6), (11, 12, 6)], drawpoints=1)
-
-        _, jpeg = cv2.imencode('.jpg', frame)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+            frame = stream.read()
+    
+            if frame is None:
+                break
+    
+            frame = cv2.flip(frame, 1)
+            img = cv2.resize(frame, (1000, 500))
+            cvzone.putTextRect(img, 'AI Push Up Counter', [345, 30], thickness=2, border=2, scale=2.5)
+            pd_pushup.findPose(img, draw=0)
+            lmlist, _ = pd_pushup.findPosition(img, draw=0, bboxWithHands=0)
+    
+            anglesp(lmlist, [lmlist[p] for p in (11, 13, 15, 12, 14, 16)], [(11, 13, 6), (13, 15, 6), (12, 14, 6),
+                                                                            (14, 16, 6), (11, 12, 6)], drawpoints=1)
+    
+            _, jpeg = cv2.imencode('.jpg', frame)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
 
 
@@ -179,8 +176,7 @@ def process_video():
     file = request.files['file']
 
     if file and allowed_file(file.filename):
-        file.save('/tmp/input_video.mp4')  # save the file to a temporary location
-        stream = cv2.VideoCapture('/tmp/input_video.mp4')  # open the file using the temporary path
+        stream = cv2.VideoCapture(file.stream)
         return Response(generate_frames(stream), mimetype='multipart/x-mixed-replace; boundary=frame')
 
         
@@ -633,9 +629,7 @@ def hi():
 
 
 
-@app.route("/pc")
-def pushup():
- return render_template("pushup.html")
+
 
 @app.route("/sc")
 def squat():
